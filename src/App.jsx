507538f438a8779938be02755c29ecc1c2ea1e61
Heli Kapadia <heli.kapadia01@gmail.com>
2024-04-12 13:53:04 +0400

    Updated app.jsx with file calls

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Pricing from "./components/Pricing";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Hero />
         <Benefits />
         <Collaboration />
+        <Services />
+        <Pricing />
+        <Footer />
       </div>
       <ButtonGradient />
     </>
